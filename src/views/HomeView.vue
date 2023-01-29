913969c538cc0cd0369f6b2181470b92ebfd225b
aldiramadani00 <alditipex123@gmail.com>
2023-01-30 03:26:24 +0700

    wewe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,9 @@
 <template>
-  
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+    <div class="home"></div>
+  </template>
+  <script>
+    export default {
+      name: 'home',
+      components: {},
+    };
+  </script>
