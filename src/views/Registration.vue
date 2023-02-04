e0b63c2f1183febe56a08dae71dc85c6e4fa5e5d
aldiramadani00 <alditipex123@gmail.com>
2023-02-05 00:45:48 +0700

    Frontend

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,18 +1,12 @@
 <template>
     <form class="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-              Nama Depan
+          <div class="w-full px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="username">
+              Username
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Supri">
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="username" type="text" placeholder="******************">
             <p class="text-red-500 text-xs italic">Harap Di isi</p>
-          </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-              Nama Belakang
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Adi">
           </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -28,10 +22,10 @@
       <br>
       <div class="flex flex-row-reverse">
         <div>
-          <router-link to="/login"><button class="bg-gray-200 p-4 rounded-3xl">Daftar</button></router-link>
+          <router-link to="/login"><button class="bg-gray-200 px-5 py-1 rounded-3xl">Daftar</button></router-link>
         </div>
         <div>
-          <router-link to="/login"><button class="bg-gray-200 p-4 rounded-3xl">Back</button></router-link>
+          <router-link to="/login"><button class="bg-gray-200 px-5 py-1 rounded-3xl right-9">Back</button></router-link>
         </div>
       </div> 
 </template>
